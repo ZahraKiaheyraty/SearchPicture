cbf8ff376519497b063759b157106c2f60bf63d8
kia <zahra.kiaheyrati1@gmail.com>
2022-01-09 16:42:01 +0330

    SetupRoomDatabase

---
+++ app/src/main/java/com/example/picturepaging3/local/UnsplashDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.picturepaging3.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.picturepaging3.local.dao.UnsplashImageDao
+import com.example.picturepaging3.local.dao.UnsplashRemoteKeysDao
+import com.example.picturepaging3.model.UnsplashImage
+import com.example.picturepaging3.model.UnsplashRemoteKeys
+
+@Database(entities = [UnsplashImage::class, UnsplashRemoteKeys::class], version = 1)
+abstract class UnsplashDatabase : RoomDatabase() {
+    abstract fun unsplashImageDao(): UnsplashImageDao
+    abstract fun unsplashRemoteKeysDao(): UnsplashRemoteKeysDao
+}
